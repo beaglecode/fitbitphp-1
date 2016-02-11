bddf62749d541e0dfad22e76be2631af88ece8df
Clay Bitner <claybitner@gmail.com>
2016-02-11 16:32:55 -0600

    adding specific log retrieval types

--- fitbit.php
+++ fitbit.php
@@ -1,4 +1,5 @@
-<?php
+<?php namespace beaglecode\fitbit;
+use \Exception;
 
 /**
  *  Basic wrapper for Fitbit([ dev.fitbit.com ]) API handles authorization, get and refresh tokens, get profile, subscribe, list subscriptions and delete subscriptions.
@@ -20,8 +21,11 @@ class Fitbit{
     protected $message = "";
     private $latestTokens = array(
         'access_token' => '',
-        'refresh_token' => ''
+        'refresh_token' => '',
+        'expires' => '',
+        'user_id' => ''
     );
+    private $subscriptions = array();
 
     public function __construct($id="", $secret="", $url=""){
         $this->id = $id;
@@ -43,6 +47,15 @@ class Fitbit{
     */
     function getLatestTokens(){
         return $this->latestTokens;
+    }
+
+    function resetTokens() {
+        $this->latestTokens = array(
+            'access_token' => '',
+            'refresh_token' => '',
+            'expires' => '',
+            'user_id' => ''
+        );
     }
 
     function setCredentials($id, $secret){
@@ -79,7 +92,9 @@ class Fitbit{
         }
         $tokens = array(
           'access_token' => $response['access_token'],
-          'refresh_token' => $response['refresh_token']
+          'refresh_token' => $response['refresh_token'],
+          'expires' => date('Y-m-d H:i:s', strtotime('+'.$response['expires_in'].' seconds')),
+          'user_id' => $response['user_id']
         );
         $this->latestTokens = $tokens;
         return $tokens;
@@ -103,14 +118,16 @@ class Fitbit{
         $response = $this->curl_request($url, $postData, $headers);
         $response = json_decode($response, true);
         if(!$this->isResponseOk($response)){
-          return false;
+            throw new FitBitException('401', $response['errors'][0]['errorType'], $response['errors'][0]['message']);
         }
         $tokens = array(
           'access_token' => $response['access_token'],
           'refresh_token' => $response['refresh_token'],
-          'expires_in' => $response['expires_in']
+          'expires' => date('Y-m-d H:i:s', strtotime('+'.$response['expires_in'].' seconds')),
+          'user_id' => $response['user_id']
         );
         $this->latestTokens = $tokens;
+        $this->updateSubscriptions();
         return $tokens;
     }
 
@@ -120,12 +137,18 @@ class Fitbit{
         $headers = array(
           "Authorization: Bearer $accessToken"
         );
-        $response = $this->curl_request($url, array(), $headers);
-        $response = json_decode($response, true);
+        $rawResponse = $this->curl_request($url, array(), $headers);
+        $response = json_decode($rawResponse, true);
         if(!$this->isResponseOk($response)){
-          return false;
+          throw new FitBitException('401', $response['errors'][0]['errorType'], $response['errors'][0]['message']);
         }
         return $response;
+    }
+
+    function updateSubscriptions() {
+        if ($this->latestTokens['access_token']) {
+            $this->subscriptions = $this->listSubscriptions($this->latestTokens['access_token']);
+        }
     }
 
 
@@ -133,32 +156,34 @@ class Fitbit{
      * adding subscription to specific user
      * @param string $accessToken
      * @param string $userId fitbit user ids
+     * @param string $subscriberId an Id for fitBit to associate your user with
      * @param array $collections resources to subscribe user
      * @return array
      */
-    function addSubscriptions($accessToken, $userId, $collections){
+    function addSubscriptions($accessToken, $userId, $subscriberId, $collections){
         $status = array();
         foreach ($collections as $collection) {
-            $subscriptionId = $this->id.'-'.$userId.'-'.$collection;
-            $url = "https://api.fitbit.com/1/user/-/$collection/apiSubscriptions/$subscriptionId.json";
-            $headers = [
-            "Authorization: Bearer $accessToken"
-            ];
-            $response = $this->curl_request($url, array(), $headers);
+            $url = "https://api.fitbit.com/1/user/-/$collection/apiSubscriptions/$subscriberId.json";
+            $headers = array(
+                "Authorization: Bearer $accessToken"
+            );
+            $response = $this->curl_request($url, array('id'=>$subscriberId), $headers, "POST");
             $response = json_decode($response, true);
             $status[$collection] =  $this->isResponseOk($response) ? true : false;
         }
+        $this->updateSubscriptions();
         return $status;
     }
 
-    function deleteSubscription($accessToken, $userId, $type){
-        $subscriptionId = $this->id.'-'.$userId.'-'.$type;
+    function deleteSubscription($accessToken, $userId, $type, $subscriptionId=null){
+        $subscriptionId = $subscriptionId ?: $this->id.'-'.$userId.'-'.$type;
         $url = "https://api.fitbit.com/1/user/-/$type/apiSubscriptions/$subscriptionId.json";
         $headers = array(
           "Authorization: Bearer $accessToken"
         );
         $response = $this->curl_request($url, array(), $headers, 'delete');
         $response = json_decode($response, true);
+        $this->updateSubscriptions();
         return $this->isResponseOk($response) ? true : false;
     }
 
@@ -190,12 +215,12 @@ class Fitbit{
     }
 
 
-    function getSubscriptionUpdatedData($accessToken, $ownerId, $date){
-        $url = "https://api.fitbit.com/1/user/$ownerId/activities/date/$date.json";
+    function getSubscriptionUpdatedData($subscriptionType, $accessToken, $ownerId, $date){
+        $url = "https://api.fitbit.com/1/user/$ownerId/$subscriptionType/date/$date.json";
         $headers = array(
           "Authorization: Bearer $accessToken"
         );
-        $response = $this->curl_request($url, array(), $headers);
+        $response = $this->curl_request($url, array(), $headers, "GET");
         $response = json_decode($response, true);
         if(!$this->isResponseOk($response)){
           return false;
@@ -204,9 +229,132 @@ class Fitbit{
     }
 
 
+    public function getActivities($accessToken, $ownerId, $date) {
+        return $this->getSubscriptionUpdatedData('activities', $accessToken, $ownerId, $date);
+    }
+
+    public function getSleep($accessToken, $ownerId, $date) {
+        return $this->getSubscriptionUpdatedData('sleep', $accessToken, $ownerId, $date);
+    }
+
+    public function getWeight($accessToken, $ownerId, $date) {
+        return $this->getSubscriptionUpdatedData('body/weight', $accessToken, $ownerId, $date.'/1d');
+    }
+
     function validateToken($accessToken){
-        $response = $this->getProfile($accessToken);
-        return $response;
+        try {
+            $response = $this->getProfile($accessToken);
+            return $response;
+        } catch (FitBitException $e) {
+            return false;
+        }
+    }
+
+    public function getTimeSeries($type, $baseDate, $toPeriod) {
+
+        switch ($type) {
+            case 'caloriesIn':
+                $path = 'foods/log/caloriesIn';
+                break;
+            case 'water':
+                $path = 'foods/log/water';
+                break;
+
+            case 'caloriesOut':
+                $path = 'activities/calories';
+                break;
+            case 'steps':
+                $path = 'activities/steps';
+                break;
+            case 'distance':
+                $path = 'activities/distance';
+                break;
+            case 'floors':
+                $path = 'activities/floors';
+                break;
+            case 'elevation':
+                $path = 'activities/elevation';
+                break;
+            case 'minutesSedentary':
+                $path = 'activities/minutesSedentary';
+                break;
+            case 'minutesLightlyActive':
+                $path = 'activities/minutesLightlyActive';
+                break;
+            case 'minutesFairlyActive':
+                $path = 'activities/minutesFairlyActive';
+                break;
+            case 'minutesVeryActive':
+                $path = 'activities/minutesVeryActive';
+                break;
+            case 'activeScore':
+                $path = 'activities/activeScore';
+                break;
+            case 'activityCalories':
+                $path = 'activities/activityCalories';
+                break;
+
+            case 'tracker_caloriesOut':
+                $path = 'activities/tracker/calories';
+                break;
+            case 'tracker_steps':
+                $path = 'activities/tracker/steps';
+                break;
+            case 'tracker_distance':
+                $path = 'activities/tracker/distance';
+                break;
+            case 'tracker_floors':
+                $path = 'activities/tracker/floors';
+                break;
+            case 'tracker_elevation':
+                $path = 'activities/tracker/elevation';
+                break;
+            case 'tracker_activeScore':
+                $path = 'activities/tracker/activeScore';
+                break;
+
+            case 'startTime':
+                $path = 'sleep/startTime';
+                break;
+            case 'timeInBed':
+                $path = 'sleep/timeInBed';
+                break;
+            case 'minutesAsleep':
+                $path = 'sleep/minutesAsleep';
+                break;
+            case 'awakeningsCount':
+                $path = 'sleep/awakeningsCount';
+                break;
+            case 'minutesAwake':
+                $path = 'sleep/minutesAwake';
+                break;
+            case 'minutesToFallAsleep':
+                $path = 'sleep/minutesToFallAsleep';
+                break;
+            case 'minutesAfterWakeup':
+                $path = 'sleep/minutesAfterWakeup';
+                break;
+            case 'efficiency':
+                $path = 'sleep/efficiency';
+                break;
+
+            case 'weight':
+                $path = 'body/weight';
+                break;
+            case 'bmi':
+                $path = 'body/bmi';
+                break;
+            case 'fat':
+                $path = 'body/fat';
+                break;
+            default:
+                return false;
+        }
+
+        $userId = $this->latestTokens['user_id'];
+        $accessToken = $this->latestTokens['access_token'];
+        $date = $baseDate . ($toPeriod ? '/'.$toPeriod : '');
+        return $this->getSubscriptionUpdatedData($path, $accessToken, $userId, $date);
     }
 
 
@@ -237,7 +385,32 @@ class Fitbit{
 
 }
 
+/**
+ * Fitbit API communication exception
+ *
+ */
+class FitBitException extends Exception {
+    public $fbMessage = '';
+    public $httpcode;
 
+    public function __construct($code, $fbMessage = null, $message = null) {
+
+        $this->fbMessage = $fbMessage;
+        $this->httpcode = $code;
+
+        if (isset($fbMessage) && !isset($message))
+            $message = $fbMessage;
+
+        try {
+            $code = (int)$code;
+        } catch (Exception $E) {
+            $code = 0;
+        }
+
+        parent::__construct($message, $code);
+    }
+
+}
 
 
 
